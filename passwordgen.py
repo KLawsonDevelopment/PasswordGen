c9e26f49b5942b94590ce652956004604234c12f
Keith Lawson <klawsondevelopment@gmail.com>
2023-03-29 11:39:08 -0400

    Update passwordgen.py
    Added comments, optimized extension

--- passwordgen.py
+++ passwordgen.py
@@ -1,9 +1,13 @@
+## Imports
+
 import random
 import string
 import os
 from pathlib import Path
 from time import sleep
 from timeit import repeat
+
+## Global Variables
 
 dir_path = Path('C:\TempPath')
 color_name = 'existColor.txt'
@@ -13,57 +17,99 @@ color_path = dir_path.joinpath(color_name)
 animal_path = dir_path.joinpath(animal_name)
 number_path = dir_path.joinpath(number_name)
 
+## Lists of colors and animals. Shorter lists are for testing.
+
 color=['Blue','Red','Yellow','Orange', 'Teal', 'Silver', 'Lime','Green','Gray']
 #color=['Red','Blue']
 animal=['bird','wolf','dog','cat','fish','lion','ferret'
         ,'snake','bee', 'fox', 'owl', 'goat', 'frog', 'bat', 'zebra', 'mouse', 'sheep', 'lamb', 'bear', 'deer', 'puma', 'orca', 'seal']
 #animal=['bird','wolf']
 
+#Initial password generation.
+
 def randomPassword(color, animal):
+
+    # If file does not exist, call file creation.
+
     if not os.path.exists("C:\TempPath\existColor.txt"):
         createFiles()
 
+    # Open files
+
     color_file = open("C:\TempPath\existColor.txt", 'w+')
     animal_file = open("C:\TempPath\existAnimal.txt", 'w+')
+
+    # Create random color and write to file
 
     randomcolor=random.choice(color)
     color_file.write(randomcolor)
     color_file.flush()
     color_file = open("C:\TempPath\existColor.txt", 'r+')
     
-    
+    # Create random animal and write to file
+
     randomanimal=random.choice(animal)
     animal_file.write(randomanimal)
     animal_file.flush()
     animal_file = open("C:\TempPath\existAnimal.txt", 'r+')
-    
+
+    # Create random numbers    
     
     randomnum1=random.choice(string.digits)
     randomnum2=random.choice(string.digits)
+
+    # Combine
     
     genPass = randomcolor+randomanimal+randomnum1+randomnum2
+
+    # If password is under 12 characters, extend with numbers.
     
-    if len(genPass) <=11:
+    while len(genPass) <=11:
         randomnumadd=random.choice(string.digits)
-        genPass+= randomnumadd
-        if len(genPass) <=11:
-            randomnumadd=random.choice(string.digits)
-            genPass+= randomnumadd
-            if len(genPass) <=11:
-                randomnumadd=random.choice(string.digits)
-                genPass+= randomnumadd
-                if len(genPass) <=11:
-                    randomnumadd=random.choice(string.digits)
-                    genPass+= randomnumadd
+        genPass+=randomnumadd
     
+    # Show password
+
     print("Password is:",genPass)
+
+    #Call anotherPassword
 
     anotherPassword(color_file,animal_file)
     
+# Checking to see if another password is needed.
     
-    
+def anotherPassword(color_file,animal_file):
+    yorn=input('Do you need another password? \n')
+    yorn=yorn.lower()
+
+    # If yes, run repeat password.
+
+    if yorn=='y' or yorn=='yes':
+        color_file.close()
+        animal_file.close()
+        repeatPassword(color,animal,color_file,animal_file)
+
+    # If no, close and delete files.
+
+    else:
+        color_file.close()
+        os.remove("C:\TempPath\existColor.txt")
+        print("Removed color file.")
+        animal_file.close()
+        os.remove("C:\TempPath\existAnimal.txt")
+        print("Removed animal file.")
+        pass
+
+# Repeat password function.
+
 def repeatPassword(color, animal,color_file,animal_file):
+
+    # Make another random color
+
     randomcolor=random.choice(color)
+
+    # Check randomcolor against color in file. If same, repeat. If different, pass.
+
     while True:
         color_file = open("C:\TempPath\existColor.txt", 'r+')
         file_data = color_file.read()
@@ -77,8 +123,12 @@ def repeatPassword(color, animal,color_file,animal_file):
             color_file = open("C:\TempPath\existColor.txt", 'r+')
             break
 
-            
+    # Create random animal
+
     randomanimal=random.choice(animal)
+
+    # Check randomanimal against animal in file. If same, repeat. If different, pass.
+
     while True:
         animal_file = open("C:\TempPath\existAnimal.txt", 'r+')
         file_data = animal_file.read()
@@ -93,42 +143,27 @@ def repeatPassword(color, animal,color_file,animal_file):
             animal_file = open("C:\TempPath\existAnimal.txt", 'r+')
             break    
         
+    # Create random numbers.
+
     randomnum1=random.choice(string.digits)
     randomnum2=random.choice(string.digits)
+
+    #Combine.
     
     genPass = randomcolor+randomanimal+randomnum1+randomnum2
-    
-    if len(genPass) <=11:
+
+    # If password is less than 12 characters, add extra digits.
+
+    while len(genPass) <=11:
         randomnumadd=random.choice(string.digits)
-        genPass+= randomnumadd
-        if len(genPass) <=11:
-            randomnumadd=random.choice(string.digits)
-            genPass+= randomnumadd
-            if len(genPass) <=11:
-                randomnumadd=random.choice(string.digits)
-                genPass+= randomnumadd
-                if len(genPass) <=11:
-                    randomnumadd=random.choice(string.digits)
-                    genPass+= randomnumadd
+        genPass+=randomnumadd
+    
+
     
     print("Password is:",genPass)
     anotherPassword(color_file,animal_file)
     
-def anotherPassword(color_file,animal_file):
-    yorn=input('Do you need another password? \n')
-    yorn=yorn.lower()
-    if yorn=='y' or yorn=='yes':
-        color_file.close()
-        animal_file.close()
-        repeatPassword(color,animal,color_file,animal_file)
-    else:
-        color_file.close()
-        os.remove("C:\TempPath\existColor.txt")
-        print("Removed color file.")
-        animal_file.close()
-        os.remove("C:\TempPath\existAnimal.txt")
-        print("Removed animal file.")
-        pass
+
     
 def createFiles():
     if dir_path.is_dir():
